6b17cd5b94489da6b833668fb0ad02f0e1657fa1
creator-of-masterpieces <timonov.dim13@gmail.com>
2025-12-12 16:17:30 +0300

    Сбросил стили; Добавил компонент MainPage;

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+const MainPage = () => {
+    return (
+        <>
+            <header>Шапка</header>
+            <main>Контент</main>
+            <footer>Подвал</footer>
+        </>
+    )
+}
+
+export default MainPage
