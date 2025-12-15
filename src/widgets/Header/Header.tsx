ad71359d26eb6922fdc6a0ab9ff366d56c2fbe21
creator-of-masterpieces <timonov.dim13@gmail.com>
2025-12-15 20:21:59 +0300

    feat(Header): Добавил компонент Header;
    # Conflicts:
    #	src/pages/MainPage.tsx

---
+++ src/widgets/Header/Header.tsx
@@ -0,0 +1,10 @@
+const Header = ()=> {
+    return(
+        <header>
+            <div>Верхняя часть шапки</div>
+            <div>Основная часть шапки</div>
+        </header>
+    )
+}
+
+export default Header;
