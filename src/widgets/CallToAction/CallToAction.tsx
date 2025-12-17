2c25752feddbe95753117a3238f4e7616b0e268f
creator-of-masterpieces <timonov.dim13@gmail.com>
2025-12-17 21:05:50 +0300

    feat(linkButton): Добавил компонент linkButton;

--- src/widgets/CallToAction/CallToAction.tsx
+++ src/widgets/CallToAction/CallToAction.tsx
@@ -1,5 +1,6 @@
 import style from './CallToAction.module.css';
 import clsx from "clsx";
+import LinkButton from "../../components/LinkButton/LinkButton.tsx";
 
 interface ICallToActionProps {
     title: string;
@@ -9,15 +10,16 @@ interface ICallToActionProps {
 }
 
 const CallToAction = (props: ICallToActionProps) => {
-    const { title, description, type ,className } = props;
+    const {title, description, type, className} = props;
     return (
-        <section className={clsx(style.cta, type === 'primary'? style.ctaPrimary: style.ctaSecondary,  className || '') }>
+        <section
+            className={clsx(style.cta, type === 'primary' ? style.ctaPrimary : style.ctaSecondary, className || '')}>
             <div className={style.container}>
                 <h1 className={style.title}>{title}</h1>
                 <p className={style.description}>{description}</p>
                 <div className={style.buttonsContainer}>
-                <button className={style.button}>Кнопка1</button>
-                <button className={style.button}>Кнопка2</button>
+                    <LinkButton className={clsx(style.button, style.buttonPrimary)} type={'link'} to={'/catalog'}>Наши работы</LinkButton>
+                    <LinkButton className={clsx(style.button, style.buttonSecondary)} type={'link'} to={'/about'}>Подробнее о бренде</LinkButton>
                 </div>
             </div>
         </section>
