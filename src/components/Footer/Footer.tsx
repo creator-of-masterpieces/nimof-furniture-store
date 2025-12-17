eb18474144a1a0259af0c5d54eff2eb51b9e2aaf
Vitaliya <vitaliya_malyutina@mail.ru>
2025-12-17 23:18:29 +0300

    feat: footer + separator

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,26 @@
 import styles from "./footer.module.css"
-import {Link} from "react-router-dom";
-
-
-
+import {Separator} from "../Separator/Separator.tsx";
 
 export const Footer = () => {
+
+    const currentYear = new Date().getFullYear()
+
     return (
         <footer className={styles.footer}>
+            <div className={styles.wrapper}>
             <ul className={styles.cardsContainer}>
                 <li className={styles.card}>
                     <h3 className={styles.cardTitle}>время работы</h3>
-                    <p className="">Понедельник - Пятница:</p>
-                    <p className="">С 9:00 до 21:00</p>
-                    <p className="">Суббота - Воскресенье:</p>
-                    <p className="">С 9:00 до 18:00</p>
+                    <div className={styles.test}>
+                        <p className="">Понедельник - Пятница:</p>
+                        <p className="">С 9:00 до 21:00</p>
+                    </div>
+                    <div className={styles.test}>
+                        <p className="">Суббота - Воскресенье:</p>
+                        <p className="">С 9:00 до 18:00</p>
+                    </div>
+
+
                 </li>
                 <li className={styles.card}>
                     <h3 className={styles.cardTitle}>наш адрес</h3>
@@ -45,6 +52,12 @@ export const Footer = () => {
                     </nav>
                 </li>
             </ul>
+            </div>
+            <Separator className={styles.separator}/>
+            <div className={styles.dataContainer}>
+                <span className={styles.footerDate}>© {currentYear} Nimof. Все права защищены.</span>
+            </div>
+
         </footer>
     )
 }
