39428125e4e1849a45d7b9324531734c0ece42fa
creator-of-masterpieces <timonov.dim13@gmail.com>
2025-12-18 16:26:21 +0300

    feat(CallToAction): Доработал компонент CallToAction;
    Усовершенствовал стилизацию вложенного компонента LinkButton;
    Добавил компоненту CallToAction пропс children;

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import style from './mainPage.module.css'
 import Header from "../../widgets/Header/Header.tsx";
 import CallToAction from "../../widgets/CallToAction/CallToAction.tsx";
+import LinkButton from "../../components/LinkButton/LinkButton.tsx";
 
 const MainPage = () => {
     return (
@@ -10,12 +11,30 @@ const MainPage = () => {
                 <CallToAction
                     title={'Мебель, с которой начинается день'}
                     description={'Откройте для себя элегантную и функциональную мебель, которая идеально впишется в ваш интерьер и подчеркнет ваш стиль.'}
-                    type={'primary'}/>
+                    type={'primary'}>
+                    <LinkButton
+                        type={'link'}
+                        appearance={'primary'}
+                        to={'/catalog'}>Наши работы</LinkButton>
+                    <LinkButton
+                                type={'link'}
+                                appearance={'secondary'}
+                                to={'/about'}>Подробнее о бренде</LinkButton>
+                </CallToAction>
                 <CallToAction
                     title={'Создайте дом своей мечты с Nimof'}
                     description={'Мы предлагаем широкий выбор качественной мебели и сантехники для вашего дома'}
                     type={'secondary'}
-                />
+                >
+                    <LinkButton
+                        type={'link'}
+                        appearance={'primary'}
+                        to={'/catalog'}>Наши работы</LinkButton>
+                    <LinkButton
+                        appearance={'secondary'}
+                        type={'button'}
+                        onClick={()=> alert('Вот тебе консультация!')}>Получить консультацию</LinkButton>
+                </CallToAction>
             </main>
             <footer>Подвал</footer>
         </div>
