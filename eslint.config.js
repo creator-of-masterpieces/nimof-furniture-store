e49e6d23e2d61661b18c677d544bb10354e03b4e
Vitaliya <vitaliya_malyutina@mail.ru>
2025-12-20 15:25:49 +0300

    настройка линта

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,45 @@
 import js from '@eslint/js'
+import ts from 'typescript-eslint'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {react, prettier},
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ts.configs.recommended,
+      ts.configs.strict,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          printWidth: 120,
+          tabWidth: 2,
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off",
+      "react/jsx-no-target-blank": 1,
+      'react-hooks/exhaustive-deps': 'error',
+      "no-console": "warn",
+      "no-debugger": "warn",
     },
   },
 ])
