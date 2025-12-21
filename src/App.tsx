49aaa4e882a0b9a25cc2678398794ec6865fb5aa
creator-of-masterpieces <timonov.dim13@gmail.com>
2025-12-21 18:31:07 +0300

    feat(ContactsPage): Добавил базовую стилизацию;

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/style.css";
 import MainPage from "./pages/MainPage/MainPage.tsx";
 import CatalogPage from "./pages/CatalogPage/CatalogPage.tsx";
-import ContactPage from "./pages/ContactPage.tsx";
+import ContactsPage from "./pages/ContactsPage/ContactsPage.tsx";
 import AboutPage from "./pages/AboutPage/AboutPage.tsx";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Route path={"/"} element={<MainPage />} />
         <Route path={"/catalog"} element={<CatalogPage />} />
         <Route path={"/about"} element={<AboutPage />} />
-        <Route path={"/contacts"} element={<ContactPage />} />
+        <Route path={"/contacts"} element={<ContactsPage />} />
       </Routes>
     </BrowserRouter>
   );
