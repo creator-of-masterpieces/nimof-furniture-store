faaabaac3172af4862237940baf2eea107edf057
creator-of-masterpieces <timonov.dim13@gmail.com>
2025-12-22 15:34:44 +0300

    feat(AboutPage): Доработал компонент AboutPage;
    Добавил внутрь компонента AboutPage секцию Наша история;
    Создал компонент SectionTitle;

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -3,6 +3,8 @@ import Header from "../../widgets/Header/Header.tsx";
 import LinkButton from "../../components/LinkButton/LinkButton.tsx";
 import CallToAction from "../../widgets/CallToAction/CallToAction.tsx";
 import { Footer } from "../../widgets/Footer/Footer.tsx";
+import SectionTitle from "../../components/SectionTitle/SectionTitle.tsx";
+import clsx from "clsx";
 
 const AboutPage = () => {
   return (
@@ -25,9 +27,32 @@ const AboutPage = () => {
           </LinkButton>
         </CallToAction>
 
-        <section>Наши история</section>
+        <section className={clsx(style.historySection, style.section)}>
+          <div className={style.historyDescription}>
+            <SectionTitle title={"Наша история"} />
+            <p className={style.historyText}>
+              Компания Nimof была основана в 2010 году группой энтузиастов, объединенных любовью к качественной мебели и
+              эргономичным решениям для жилых и рабочих пространств. За эти годы мы выросли из небольшой мастерской в
+              ведущего производителя мебели в Москве и Московской области.
+            </p>
+            <p className={style.historyText}>
+              Мы гордимся тем, что наша мебель находится в тысячах домов и офисов, делая жизнь людей комфортнее и
+              удобнее. Наша цель — не просто продавать мебель, а создавать уникальные решения, которые идеально
+              вписываются в пространство и отвечают всем потребностям наших клиентов.
+            </p>
+          </div>
 
-        <section>Как мы работаем</section>
+          <div className={style.historyMedia}>
+            <img
+              src="/src/assets/images/living-room.jpg"
+              alt="Фото хорошо освещенной гостинной в зеленых тонах с бежевым диваном в центре"
+            />
+          </div>
+        </section>
+
+        <section>
+          <SectionTitle title={"Как мы работаем"} />
+        </section>
       </main>
 
       <Footer />
